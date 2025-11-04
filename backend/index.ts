3207b76fdd9dd4bfd79e7d8810d2412ed97fc29d
Andreas <andreas.johansson@chasacademy.se>
2025-11-04 09:44:10 +0100

    initial setup

---
+++ backend/index.ts
@@ -0,0 +1,12 @@
+export interface Example {
+    id: number;
+    name: string;
+    description?: string;
+}
+
+export interface User {
+    id: number;
+    username: string;
+    email: string;
+    password: string;
+}
