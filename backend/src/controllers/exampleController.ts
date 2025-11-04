d9bc2494969af725beaa5219eaa510fe017138e9
Erik Andersson <erik.andersson@chascademy.se>
2025-11-04 10:41:20 +0100

    start

--- backend/src/controllers/exampleController.ts
+++ backend/src/controllers/exampleController.ts
@@ -1,13 +1,32 @@
 import { Request, Response } from 'express';
 
+const testDB = [{id: 1, message: "krasch"}, {id: 2, message: "överbelastning"}]
+
 export class IncidentController {
-    public getAllIncidents(req: Request, res: Response): void {
-        // Logic to retrieve an example
-        res.send('Get example');
+    getAllIncidents(req: Request, res: Response) {
+
+        try {
+            if (testDB.length === 0) return res.status(404).json({message: "no incidents"})
+            return res.status(200).json({data: testDB})
+        } catch (error) {
+            res.status(500).json({message: "DB error"})
+        }
     }
 
-    public createNewIncident(req: Request, res: Response): void {
-        // Logic to create an example
+    async createNewIncident(req: Request, res: Response) {
+        const {title, description} = req.body
+        try {
+            const aiResult = await analyzeIncident(title, description)
+            
+            const newIncident = {
+                title,
+                description,
+                //ai saker
+            }
+            
+        } catch (error) {
+            
+        }
         res.send('Create example');
     }
 }
